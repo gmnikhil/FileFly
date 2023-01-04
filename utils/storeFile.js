a43db3219a957001b503d4fc1db0725c165765a3
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2023-01-04 22:31:29 +0530

    revoked key + placeholder

--- utils/storeFile.js
+++ utils/storeFile.js
@@ -5,7 +5,7 @@ import mime from "mime";
 import path from "path";
 
 const NFT_STORAGE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVhYjY3QjA5NDAwRjk3RGU1NTA4MzQ2MUQ2ZjFjMGUxQjEzMmQ1QWQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1NzM4MzYzNjA3NSwibmFtZSI6ImhlYWx0aC1uZXQifQ.YZQnNhp2lHq7m9K3uuM2J4sYQ7D9jsa3TeE74D4xc4Y";
+  "nft_storage_key";
 
 async function storeNFT(image, name, description) {
   //const image = await fileFromPath(imagePath)
